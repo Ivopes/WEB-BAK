ae15927b2d3d354d5826d64778ab27fa04b0fa98
Ivo Pesak <hapesak@seznam.cz>
2021-04-26 15:37:19 +0200

    delete data on forced logout

--- src/app/main/shared/services/auth-guard.service.ts
+++ src/app/main/shared/services/auth-guard.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { SongService } from './song.service';
+import { PlaylistService } from './playlist.service';
+import { AccountService } from './account.service';
+import { StorageService } from './storage.service';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(
     private jwtHelper: JwtHelperService,
-    private router: Router
+    private router: Router,
+    private songService: SongService,
+    private playlistService: PlaylistService,
+    private accountService: AccountService,
+    private storageService: StorageService,
+
     ) { }
   canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     return this.canActivate();
@@ -18,6 +27,11 @@ export class AuthGuard implements CanActivate, CanActivateChild {
     if (token && !this.jwtHelper.isTokenExpired(token)){
       return true;
     }
+    localStorage.removeItem('jwt');
+    this.songService.clearData();
+    this.playlistService.clearData();
+    this.accountService.clearData();
+    this.storageService.clearData();
     this.router.navigate(['/login']);
     return false;
   }
